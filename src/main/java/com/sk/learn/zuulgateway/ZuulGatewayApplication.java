e78179117130871a9dde3025e360d43b854427f1
Sushant <sushantkr16@gmail.com>
2018-11-01 12:52:13 +0530

    zuul gateway

---
+++ src/main/java/com/sk/learn/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.sk.learn.zuulgateway;
+
+import com.sk.learn.zuulgateway.filters.pre.ZuulPreFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public ZuulPreFilter zuulPreFilter() {
+		return new ZuulPreFilter();
+	}
+}
